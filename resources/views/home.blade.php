1e41f9ba8b193a30f5a5fefa9cbc403b42f13b93
Casweeney <casweeno2000@gmail.com>
2020-02-02 00:01:23 +0100

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,234 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John Doe</p>
+                                <p class="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-8" id="message">
+            <div class="message-wrapper">
+                <ul class="messages">
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>lorem ipsum dolor.</p>
+                            <p class="date">1 Sep, 2020</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="input-text">
+                <input type="text" name="message" class="submit">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
